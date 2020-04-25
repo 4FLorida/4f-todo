5cb1bfe3e6ac45c61f768ea045cd4fd9f38cebee
ZerrinBostan <46298014+ZerrinBostan@users.noreply.github.com>
2020-04-25 22:35:28 +0300

    unnecessary line of code deleted.

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,6 @@ module.exports = {
     "rules": {
         "semi":["error", "always"],
         "quotes": ["error", "single"],
-        // "indent": ["error", 4],
         "prefer-const": "error",
         "comma-dangle": ["error", "always-multiline"],
         "prettier/prettier": ["error"],
