97479ed5fde689bb80d39d9629804dcb6ebaf224
Berkay Burak <berkay.burak.misirlioglu@gmail.com>
2020-04-26 01:59:07 +0300

    Revert "example3"
    This reverts commit 0d40d409a844693f3b2a604c144a808cdf1c533d.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const Home = () => {
             <h1>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita ipsum, rerum! Aliquid, aut beatae delectus deserunt dolor dolore esse est, facilis in ipsa itaque laborum laudantium minus molestiae natus nemo nihil nostrum nulla officia pariatur perspiciatis praesentium provident
                 qui quisquam recusandae reiciendis rem saepe sapiente sequi suscipit tenetur velit veritatis voluptatum! Blanditiis dolor enim est facilis illo inventore ipsum magnam mollitia pariatur perspiciatis qui, quidem ratione reiciendis. Amet animi autem culpa deserunt dolorem, enim id,
-                laboriosam laudantium maiores, minus nesciunt nisi non odio perspiciatis reprehenderit rerum sed? Atque ducimus maiores nesciunt nisi odio odit, saepe ut? Quas quis sunt vitae.
+                laboriosam laudantium maiores, minus nesciunt nisi non odio perspiciatis reprehenderit rerum sed? Atque ducimus maiores nesciunt nisi odio odit, saepe ut? Quas quis sunt vitae. Allah belanı vwersin github
             </h1>
         </Wrapper>
     );
